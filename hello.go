b4566aaaa734e76d98e75a26c731dbe0cfb1d1f1
Scott Graham <scott.hgrc@h4ck3r.net>
2010-03-26 00:55:42 -0700

    very initial "program" and learning about pointers in go

--- hello.go
+++ hello.go
@@ -52,34 +52,77 @@ func (t Type) String() string {
     return "<unknown>"
 }
 
-func (p Pair) IsInUse() bool { return p.car & 0x80000000 != 0 }
-func (p Pair) SetInUse() () { p.car |= 0x80000000 }
-func (p Pair) ClrInUse() () { p.car &^= 0x80000000 }
+func (p *Pair) InUse() bool { return p.car & 0x80000000 != 0 }
+func (p *Pair) SetInUse() () { p.car |= 0x80000000 }
+func (p *Pair) ClrInUse() () { p.car &^= 0x80000000 }
 
-func (p Pair) IsCarBeingTraced() bool { return p.cdr & 0x80000000 != 0 }
-func (p Pair) SetCarBeingTraced() () { p.cdr |= 0x80000000 }
-func (p Pair) ClrCarBeingTraced() () { p.cdr &^= 0x80000000 }
+func (p *Pair) CarBeingTraced() bool { return p.cdr & 0x80000000 != 0 }
+func (p *Pair) SetCarBeingTraced() () { p.cdr |= 0x80000000 }
+func (p *Pair) ClrCarBeingTraced() () { p.cdr &^= 0x80000000 }
 
-func (p Pair) CarType() Type { return Type((p.car & 0x7fffffff) >> 24) }
-func (p Pair) CdrType() Type { return Type((p.cdr & 0x7fffffff) >> 24) }
+func (p *Pair) CarType() Type { return Type((p.car & 0x7fffffff) >> 24) }
+func (p *Pair) SetCar(t Type, data uint32) { p.car = uint32(t << 24) | data }
+func (p *Pair) SetCarNil() { p.SetCar(t_pointer, 0) }
 
-func (p Pair) CarData() uint { return uint(p.car & 0xffffff) }
-func (p Pair) CdrData() uint { return uint(p.cdr & 0xffffff) }
+func (p *Pair) CdrType() Type { return Type((p.cdr & 0x7fffffff) >> 24) }
+func (p *Pair) SetCdr(t Type, data uint32) { p.cdr = uint32(t << 24) | data }
+func (p *Pair) SetCdrNil() { p.SetCdr(t_pointer, 0) }
 
-func (p Pair) String() string {
-    return fmt.Sprintf("<InUse:%t, GC:%t, A is:%s, D is:%s, A:%0x, D:%0x>",
-            p.IsInUse(), p.IsCarBeingTraced(),
-            p.CarType().String()[2:], p.CdrType().String()[2:],
-            p.CarData(), p.CdrData())
+func (p *Pair) Set(carT Type, carData uint32, cdrT Type, cdrData uint32, inUse bool) {
+    p.SetCar(carT, carData)
+    p.SetCdr(cdrT, cdrData)
+    if inUse {
+        p.SetInUse()
+    } else {
+        p.ClrInUse()
+    }
 }
 
+func (p *Pair) CarData() uint { return uint(p.car & 0xffffff) }
+func (p *Pair) CdrData() uint { return uint(p.cdr & 0xffffff) }
+
+func (p Pair) String() string {
+    return fmt.Sprintf("<InUse:%t, GC:%t, A~%s, D~%s, A=%06x (%d), D=%06x (%d)>",
+            p.InUse(), p.CarBeingTraced(),
+            p.CarType().String()[2:], p.CdrType().String()[2:],
+            p.CarData(), p.CarData(),
+            p.CdrData(), p.CdrData())
+}
+
+const maxAddress = 0xffffff
 
 type MachineData struct {
-    memory [0xffffff]Pair
+    memory [maxAddress]Pair
+    exp Pair
+    env Pair
+    val Pair
+    args Pair
+    clink Pair
+
+    pc uint
 }
 
 func main() {
     md := new(MachineData)
-    a := 5 + 5
-    fmt.Printf("hello, world: %d %v\n", a, md.memory[0])
+
+    md.memory[0].Set(t_if, 1, t_pointer, 0, true)
+
+    md.memory[1].Set(t_fixnum, 100, t_pointer, 2, true)
+
+    md.memory[2].Set(t_fixnum, 200, t_fixnum, 300, true)
+
+
+    for {
+        cur := md.memory[md.pc]
+        switch cur.CarType() {
+            case t_if:
+                fmt.Printf("%06x: in t_if: %v\n", md.pc, cur)
+            default:
+                fmt.Printf("%06x: unknown (%v)\n", md.pc, cur)
+        }
+        md.pc += 1
+        if md.pc > maxAddress {
+            md.pc = 0
+        }
+    }
 }
