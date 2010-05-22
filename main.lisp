09251ee5f74d544052f22f94f120731b5ebc6929
Scott Graham <scott.hgrc@h4ck3r.net>
2010-05-21 22:40:28 -0700

    improve sdot'r, simplify 'if' layout to avoid confusing myself so much

--- main.lisp
+++ main.lisp
@@ -33,15 +33,15 @@ remaining 12 bits allow address of 4k cells == 16k bytes
 
 (declaim (optimize (safety 3)) (optimize (debug 3)) (optimize (speed 0)))
 
-(defparameter *type-self-eval-ptr*   #b0000000000000000)
-(defparameter *type-if*              #b0001000000000000)
-(defparameter *type-quote*           #b0010000000000000)
-(defparameter *type-car*             #b0011000000000000)
-(defparameter *type-cdr*             #b0100000000000000)
+(defparameter *type-is-ptr-mask*     #b1000000000000000)
 
-(defparameter *type-self-eval-immed* #b1000000000000000)
+(defparameter *type-self-eval-ptr*   #b1000000000000000)
+(defparameter *type-if*              #b1001000000000000)
+(defparameter *type-quote*           #b1010000000000000)
+(defparameter *type-car*             #b1011000000000000)
+(defparameter *type-cdr*             #b1100000000000000)
 
-(defparameter *type-is-atom-mask*    #b1000000000000000)
+(defparameter *type-self-eval-immed* #b0000000000000000)
 
 (defparameter *type-mask*            #b1111000000000000)
 (defparameter *data-mask*            #b0000111111111111)
@@ -53,12 +53,17 @@ remaining 12 bits allow address of 4k cells == 16k bytes
 (defvar *reg-alloc* 0)
 (defvar *memory* (make-array 4096))
 
-(defun prim-atom? (exp)
-  (not (= (logand exp *type-is-atom-mask*) 0)))
+(defun prim-list? (p) (and
+                        (= (logand p *type-is-ptr-mask*) *type-is-ptr-mask*)
+                        (not (= p *type-self-eval-ptr*)))) ; nil
+(defun prim-atom? (p) (not (prim-list? p)))
+(defun prim-if? (p) (= (logand p *type-if*) *type-if*))
 
+(defun prim-get-data (p) (logand p *data-mask*))
+
+(defun prim-car-data (exp)
+  (logand (prim-car exp) *data-mask*))
 (defun prim-car (exp)
-  (logand (prim-car-raw exp) *data-mask*))
-(defun prim-car-raw (exp)
   (assert (not (prim-atom? exp)))
   (ash
     (logand
@@ -66,10 +71,9 @@ remaining 12 bits allow address of 4k cells == 16k bytes
       *car-mask*)
     -16))
 
+(defun prim-cdr-data (exp)
+  (logand (prim-cdr exp) *data-mask*))
 (defun prim-cdr (exp)
-  (logand (prim-cdr-raw exp) *data-mask*))
-
-(defun prim-cdr-raw (exp)
   (assert (not (prim-atom? exp)))
   (logand
     (elt *memory* (logand exp *data-mask*))
@@ -79,7 +83,8 @@ remaining 12 bits allow address of 4k cells == 16k bytes
   (let ((loc *reg-alloc*))
     (setf (elt *memory* loc) (logior (ash kar 16) kdr))
     (incf *reg-alloc*)
-    loc))
+    (logior *type-self-eval-ptr* loc)))
+
 
 (defvar *prim-nil* (prim-cons 0 0))
 (defvar *prim-t* (prim-cons 0 0))
@@ -113,11 +118,15 @@ remaining 12 bits allow address of 4k cells == 16k bytes
            (let ((if-cond (cadr exp))
                  (if-then (caddr exp))
                  (if-else (cadddr exp)))
-             (let ((tail (prim-cons
-                           (scompile if-then)
-                           (scompile if-else)))
-                   (test (scompile if-cond)))
-               (logior *type-if* (prim-cons test tail)))))
+             (write if-cond)
+             (write if-then)
+             (write if-else)
+             (let ((conditional (scompile if-cond))
+                   (consequent (scompile if-then))
+                   (alternative (scompile if-else)))
+               (logior *type-if* (prim-cons conditional
+                                            (prim-cons consequent
+                                                       (prim-cons alternative *prim-nil*)))))))
           ((eq (car exp) 'car)
            (logior *type-car* (scompile (cadr exp))))
           ((eq (car exp) 'cdr)
@@ -134,95 +143,74 @@ remaining 12 bits allow address of 4k cells == 16k bytes
     ((eq sexp 0)
      '(:nil))
     ((eq (logand *type-mask* sexp) *type-if*)
-     `(:if ,(spprint (prim-car-raw sexp))
-           ,(spprint (prim-car-raw (prim-cdr-raw sexp)))
-           ,(spprint (prim-cdr-raw (prim-cdr-raw sexp)))))
+     `(:if ,(spprint (prim-car sexp))
+           ,(spprint (prim-car (prim-cdr sexp)))
+           ,(spprint (prim-cdr (prim-cdr sexp)))))
     ((eq (logand *type-mask* sexp) *type-car*)
-     `(:car ,(spprint (prim-car-raw sexp))))
+     `(:car ,(spprint (prim-car sexp))))
     ((eq (logand *type-mask* sexp) *type-cdr*)
-     `(:cdr ,(spprint (prim-car-raw sexp))))
+     `(:cdr ,(spprint (prim-car sexp))))
     (t (error "unhandled case" sexp))))
+
+(defun get-attached-nodes (p &optional ret)
+  (if (prim-list? p)
+    (get-attached-nodes (prim-cdr p)
+                        (get-attached-nodes (prim-car p) (cons p ret)))
+    ret))
+
+(defun node-label (p)
+  (cond ((eq p *type-self-eval-ptr*)
+         "(nil)")
+        ((prim-atom? p)
+         (format nil "INT ~a" (prim-get-data p)))
+        ((prim-if? p)
+         (format nil "IF (0x~x)" p))
+        (t (format nil "UNK 0x~x" p))))
+
+(defun node->dot (p)
+  (if (prim-atom? p)
+    (node-label p)
+    (format nil "\"0x~x\" [ label = \"<addr> @ 0x~x | <car> ~a | <cdr> ~a\" shape = \"record\" ];~%"
+            p
+            p
+            (if (prim-atom? (prim-car p))
+              (node-label (prim-car p))
+              (format nil "0x~x" (prim-car p)))
+            (if (prim-atom? (prim-cdr p))
+              (node-label (prim-cdr p))
+              (format nil "0x~x" (prim-cdr p))))))
+
+(defun node-connections (p)
+  (let ((ret nil))
+    (if (and (prim-list? p) (prim-list? (prim-car p)))
+      (setq ret (cons (format nil "\"0x~x\":car -> \"0x~x\":addr;~%" p (prim-car p)) ret)))
+    (if (and (prim-list? p) (prim-list? (prim-cdr p)))
+      (setq ret (cons (format nil "\"0x~x\":cdr -> \"0x~x\":addr;~%" p (prim-cdr p)) ret)))
+    (reduce (lambda (a b) (concatenate 'string a b)) ret :initial-value "")))
 
 (defun sdot (sexp)
   "convert simple machine expr into graphviz .dot text format"
   (with-open-file (out "tmp.dot" :direction :output :if-exists :supersede)
     (format out "digraph nodes {~%")
     (format out "graph [~%rankdir = \"LR\"~%shape = \"record\"~%];~%")
-    (labels ((sdot-node (sexp)
-                        (flet ((head (sexp)
-                                     (format out "~x [~%" sexp))
-                               (tail ()
-                                     (format out "~%shape = \"record\"~%];~%")))
-                          (head sexp)
-                          (cond
-                            ((eq (logand *type-mask* sexp) *type-self-eval-immed*)
-                             (format out "label = \"<head> int ~d\"" (logand sexp *data-mask*)))
-                            ((eq sexp 0)
-                             (format out "label = \"<head> (nil)\""))
-                            ((eq (logand *type-mask* sexp) *type-if*)
-                             ; a bit of extra work for if because we walkt to the then-else too
-                             (format out "label = \"<head> if ~x | <car> car = ~x | <cdr> cdr = ~x\""
-                                     sexp
-                                     (prim-car-raw sexp)
-                                     (prim-cdr-raw sexp))
-                             (tail)
-                             (format out "~x:car -> ~x:head~%" sexp (prim-car-raw sexp))
-                             (format out "~x:cdr -> ~x:head~%" sexp (prim-cdr-raw sexp))
-                             (head (prim-cdr-raw sexp))
-                             (let ((te-sexp (prim-cdr-raw sexp)))
-                               (format out "label = \"<head> then-else ~x | <car> car = ~x | <cdr> cdr = ~x\""
-                                       te-sexp
-                                       (prim-car-raw te-sexp)
-                                       (prim-cdr-raw te-sexp))
-                               (tail)
-                               (format out "~x:car -> ~x:head~%" te-sexp (prim-car-raw te-sexp))
-                               (format out "~x:cdr -> ~x:head~%" te-sexp (prim-cdr-raw te-sexp)))
-                             (sdot-node (prim-car-raw sexp))
-                             (sdot-node (prim-car-raw (prim-cdr-raw sexp)))
-                             (sdot-node (prim-cdr-raw (prim-cdr-raw sexp)))
-                             (head sexp))
-                            (t (error "unhandled case" sexp)))
-                          (tail))))
-      (sdot-node sexp))
-    (format out "}~%")
-    ))
+    (let* ((nodelist (get-attached-nodes sexp))
+           (nodes-as-string (mapcar #'node->dot nodelist))
+           (connections (mapcar #'node-connections nodelist)))
+      (loop for node in nodes-as-string do (princ node out))
+      (loop for cnx in connections do (princ cnx out)))
+    (format out "start [ label = \"~a\" ];~%" (node-label sexp))
+    (if (prim-list? sexp)
+      (format out "start -> \"0x~x\":addr;~%"  sexp))
+    (format out "}~%")))
 
 (defun sdot-and-view (sexp)
-  "sdot and then run-program to generate and view as png"
+  "sdot and then run-program to generate and view png"
   (sdot sexp)
   (sb-ext:run-program "/bin/sh" '("-c" "dot -Tpng tmp.dot -otmp.png && gnome-open tmp.png")
                       :output t
                       :error :output))
 
-(sdot-and-view (scompile 1))
-(sdot-and-view (scompile '(if 1 2 3)))
+;(sdot-and-view (scompile 99))
+;(sdot-and-view (scompile '(if 1 2 3)))
+;(sdot-and-view (scompile '(if 1 2)))
 
-
-
-#|
-    FILE* f = fopen("tmp.dot", "wt");
-    fprintf(f, "digraph nodes {\n");
-    fprintf(f, "graph [\nrankdir = \"LR\"\n];\n");
-    for (int i = StartOfMemory; i < sAllocPoint; ++i)
-    {
-        fprintf(f,
-                "\"0x%x\" [\n  label = \"<addr> 0x%x (%s%s) | <car> car = ",
-                i,
-                i,
-                IsMarked(i) ? "M" : "",
-                IsCdrTraceInProgress(i) ? "T" : "");
-        if (Car(i) == 0) fprintf(f, "(nil)");
-        else fprintf(f, "0x%x", Car(i));
-        fprintf(f, "| <cdr> cdr = ");
-        if (Cdr(i) == 0) fprintf(f, "(nil)");
-        else fprintf(f, "0x%x", Cdr(i));
-        fprintf(f, "\"\n  shape = \"record\"\n];\n");
-        if (Car(i) != 0)
-            fprintf(f, "\"0x%x\":car -> \"0x%x\":addr;\n", i, Car(i));
-        if (Cdr(i) != 0)
-            fprintf(f, "\"0x%x\":cdr -> \"0x%x\":addr;\n", i, Cdr(i));
-    }
-    fprintf(f, "}\n");
-    fclose(f);
-    system("dot -Tpng tmp.dot -otmp.png && gnome-open tmp.png");
-|#
