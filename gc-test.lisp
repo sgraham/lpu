c1cabb141cb59c101eb159041c752ff3ca5a8558
Scott Graham <scott.hgrc@h4ck3r.net>
2010-05-30 14:50:59 -0700

    add (broken) circular gc test

--- gc-test.lisp
+++ gc-test.lisp
@@ -20,6 +20,24 @@
       (assert-equal 5 *reg-alloc*))))
 
 
+(define-test gc-loop
+  ; circular a->b->c->a->...
+  (reset-machine)
+  (assert-equal 0 *reg-cur-halfspace*)
+  (let* ((c (prim-cons *prim-nil* *prim-nil*))
+         (b (prim-cons c *prim-nil*))
+         (a (prim-cons b *prim-nil*)))
+    (assert-equal 5 *reg-alloc*)
+    (prim-rplaca c a)
+    (let ((before (spprint a)))
+      (setq *reg-val* a)
+      (funcall *gc-func*)
+      (setq a *reg-val*)
+      (let ((after (spprint a)))
+        (assert-equal before after)
+        (assert-equal 1 *reg-cur-halfspace*)
+        (assert-equal 5 *reg-alloc*)))))
+
 
 
 (run-tests)
