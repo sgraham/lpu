6ea8e490977f8cc2d0d7af13393bbd05f16d7bb8
Scott Graham <scott.hgrc@h4ck3r.net>
2010-06-12 00:54:36 -0700

    some work towards writing a simple sim

--- opcodes.lisp
+++ opcodes.lisp
@@ -5,7 +5,7 @@
 ; the register and memory operations are specified with bit ranges from high to low.
 ;
 ; user registers are A, D, P, all 16 bits
-; inaccessible registers: HEAP (12 bits), HS (1 bit), IP (14 bits), INSTR (8 bits)
+; inaccessible registers: HEAP (12 bits), HS (1 bit), PC (14 bits), IR (8 bits)
 ;
 ; see also comments in gc.asm for layout of heap which corresponds to some constants here
 ;
@@ -17,30 +17,137 @@
 
 (defvar *name-to-opcode* (make-hash-table))
 
+(defclass opdata ()
+  ((name
+     :initarg :name
+     :reader name)
+   (opcode
+     :initarg :opcode
+     :reader opcode)
+   (body
+     :initarg :body
+     :reader body)))
+
 (defmacro op (opcode mnemonic &body body)
-  `(setf (gethash ,mnemonic *name-to-opcode*) ,opcode))
+  `(setf (gethash ,mnemonic *name-to-opcode*)
+         (make-instance 'opdata 
+                        :name ,mnemonic
+                        :opcode ,opcode
+                        :body ',body)))
 
 (defmacro pseudo (mnemonic &body body)
   )
 
+(defmacro reg-getter (reg func width)
+  `(defun ,func (high low)
+     (assert (>= high low))
+     (let ((start (- (1- ,width) high))
+           (end (1+ (- (1- ,width) low))))
+       (subseq ,reg start end))))
+
+(defmacro reg-setter (reg func width)
+  `(defun ,func (high low val)
+     (assert (>= high low))
+     (assert (= (length val) (1+ (- high low))))
+     (let ((start (- (1- ,width) high))
+           (end (1+ (- (1- ,width) low))))
+       (setf (subseq ,reg start end) val))))
+
+(defmacro defregister (name width getter setter)
+ `(progn
+    (defvar ,name (make-array ,width :element-type 'bit))
+    (reg-getter ,name ,getter ,width)
+    (reg-setter ,name ,setter ,width)))
+
+(defregister reg-a 16 getA setA)
+(defregister reg-d 16 getD setD)
+(defregister reg-p 16 getP setP)
+(defregister reg-heap 12 getHEAP setHEAP)
+(defregister reg-hs 1 getHS setHS)
+(defregister reg-pc 14 getPC setPC)
+(defregister reg-ir 8 getIR setIR)
+
+(defregister reg-mem (* 8 4 4096 2) getMEM setMEM) ; 8 bits, 4 bytes per cell, 4k cells, 2 hs
+(assert (= (length reg-mem) 262144)) ; just to be sure since that's what the sram is
+
+(defun bits-for-offset (offset)
+  (assert (and (>= offset 0)
+               (< offset 4)))
+  (cond ((= offset 0) #*00)
+        ((= offset 1) #*01)
+        ((= offset 2) #*10)
+        ((= offset 3) #*11)))
+
+(defun bits-to-integer (bits)
+  (reduce #'+ (let ((n 1))
+                (reverse
+                  (map 'vector
+                       #'(lambda (x)
+                           (let ((ret (* x n)))
+                             (setq n (* n 2))
+                             ret))
+                       (reverse bits))))))
+
+(defun readram (offset)
+  "always indexed by P + byte offset + halfspace offset, and in 8 bit chunks"
+  (let* ((addr (bits-to-integer (concatenate 'vector (getHS 0 0) (getP #xB #x0) (bits-for-offset offset))))
+         (bit-addr-low (* addr 8))
+         (bit-addr-high (+ bit-addr-low 7)))
+   (getMEM bit-addr-high bit-addr-low)))
+
+(defun writeram (offset val)
+ "always indexed by P + byte offset + halfspace offset, and in 8 bit chunks"
+  (assert (and (>= offset 0)
+               (< offset 4)))
+  (let* ((addr (bits-to-integer (concatenate 'vector (getHS 0 0) (getP #xB #x0) (bits-for-offset offset))))
+         (bit-addr-low (* addr 8))
+         (bit-addr-high (+ bit-addr-low 7)))
+   (setMEM bit-addr-high bit-addr-low val)))
+
+
+;(maphash #'(lambda (k v) (format t "~a => ~a~%" k v)) *name-to-opcode*)
 
 (op #b00000000 'nop)
-(op #b00000001 'car)
-(op #b00000010 'cdr)
-(op #b00000011 'load)
+(op #b00000001 'car
+  (setA #x7 #x0 (readram 0))
+  (setA #xF #x8 (readram 1))
+(op #b00000010 'cdr
+  (setD #x7 #x0 (readram 2))
+  (setD #xF #x8 (readram 3))
+;(op #b00000011 unassigned)
 (op #b00000100 'cons)
-(op #b00000101 'rplaca)
-(op #b00000110 'rplacd)
-(op #b00000111 'rplacb)
+(op #b00000101 'rplaca
+  (writeram 0 (getA #x7 #x0))
+  (writeram 1 (getA #xF #x8)))
+(op #b00000110 'rplacd
+  (writeram 2 (getD #x7 #x0))
+  (writeram 3 (getD #xF #x8)))
+;(op #b00000111 unassigned)
 
-(op #b00001000 'shl4)
-(op #b00001001 'shr4)
-(op #b00001010 'shl12)
-(op #b00001011 'shr12)
-(op #b00001100 'shl)
-(op #b00001101 'shr)
-;(op #b00001110 unassigned)
-;(op #b00001111 unassigned)
+(op #b00001000 'shl4
+  (setA #xF #x4 (getA #xB #x0))
+  (setA #x3 #x0 (getZero 4)))
+(op #b00001001 'shr4
+  (setA #xB #x0 (getA #xF #x4))
+  (setA #xF #xC (getZero 4)))
+(op #b00001010 'shl12
+  (setA #xF #xC (getA #x3 #x0))
+  (setA #xB #x0 (getZero 12)))
+(op #b00001011 'shr12
+  (setA #x3 #x0 (getA #xF #xC))
+  (setA #xF #x4 (getZero 12)))
+(op #b00001100 'shl
+  (setA #xF #x1 (getA #xE #x0))
+  (setA #x0 #x0 (getZero 1)))
+(op #b00001101 'shr
+  (setA #xE #x0 (getA #xF #x1))
+  (setA #xF #xF (getZero 1)))
+(op #b00001110 'shlnum
+  (setA #xB #x1 (getA #xA #x0))
+  (setA #x0 #x0 (getZero 1)))
+(op #b00001111 'shrnum
+  (setA #xA #x0 (getA #xB #x1))
+  (setA #xB #xB (getZero 1)))
 
 (op #b00010000 'add)
 (op #b00010001 'sub)
@@ -86,34 +193,38 @@
 ; todo; some of jz0/jz1/jz-1 don't make sense and could be reused
 ; one is a nop, could reuse 00000000 opcode
 (loop for i from -32 to 31 do
- (op (logior #b01000000 (logand i #b111111))
-   (intern (concatenate 'string (symbol-name 'jz) (write-to-string i)))))
+      (op (logior #b01000000 (logand i #b111111))
+        (intern (concatenate 'string (symbol-name 'jz) (write-to-string i)))))
 
 (loop for i from 0 to 63 do
- (op (logior #b10000000 i)
-   (intern (concatenate 'string (symbol-name 'immlo) (write-to-string i)))))
+      (op (logior #b10000000 i)
+        (intern (concatenate 'string (symbol-name 'immlo) (write-to-string i)))))
 
 (loop for i from 0 to 63 do
- (op (logior #b11000000 i)
-   (intern (concatenate 'string (symbol-name 'immhi) (write-to-string i)))))
+      (op (logior #b11000000 i)
+        (intern (concatenate 'string (symbol-name 'immhi) (write-to-string i)))))
+
+(pseudo load ; load both car and cdr
+  car
+  cdr)
 
 (pseudo isbh ; top bit (broken heart) is set?
- shr12
- shl
- shr4)
+  shr12
+  shl
+  shr4)
 
 (pseudo islist ; second-from-top bit (is ptr) is set?
- shl
- shr12
- shl
- shr4)
+  shl
+  shr12
+  shl
+  shr4)
 
 (pseudo isatom ; second-from-top bit (is ptr) is clear?
- shl
- shr12
- shl
- shr4
- not)
+  shl
+  shr12
+  shl
+  shr4
+  not)
 
 ; todo; would be nice to put these in hardware a little more somehow
 ; walk proper list in P to a constant depth and get element to A
@@ -386,6 +497,5 @@ pseudo setl7
     rplaca
 |#
 
-(maphash #'(lambda (k v) (format t "~a => ~a~%" k v)) *name-to-opcode*)
 
 ; vim: set lispwords+=op,pseudo:
